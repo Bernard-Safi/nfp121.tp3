0656cc798fd1689b685596e137604d9cbab23e63
Bernard-Safi <bernard.safi@isae.edu.lb>
2021-06-27 09:59:51 +0300

    27/6/2021

--- tp3/question2/Pile4.java
+++ tp3/question2/Pile4.java
@@ -6,122 +6,122 @@ import question1.PileVideException;
 import java.util.Stack;
 
 public class Pile4 implements PileI, Cloneable {
-	/** la liste des Maillons/Elements */
-	private Maillon stk;
-	/** la capacité de la pile */
-	private int capacite;
-	/** le nombre */
-	private int nombre;
+    /** la liste des Maillons/Elements */
+    private Maillon stk;
+    /** la capacité de la pile */
+    private int capacite;
+    /** le nombre */
+    private int nombre;
 
-	/**
-	 * Classe interne "statique" contenant chaque élément de la chaine c'est une
-	 * proposition, vous pouvez l'ignorer !
-	 */
-	private static class Maillon implements Cloneable {
-		private Object element;
-		private Maillon suivant;
+    /**
+     * Classe interne "statique" contenant chaque élément de la chaine c'est une
+     * proposition, vous pouvez l'ignorer !
+     */
+    private static class Maillon implements Cloneable {
+        private Object element;
+        private Maillon suivant;
 
-		public Maillon(Object element, Maillon suivant) {
-			this.element = element;
-			this.suivant = suivant;
-		}
+        public Maillon(Object element, Maillon suivant) {
+            this.element = element;
+            this.suivant = suivant;
+        }
 
-		public Maillon suivant() {
-			return this.suivant;
-		}
+        public Maillon suivant() {
+            return this.suivant;
+        }
 
-		public Object element() {
-			return this.element;
-		}
+        public Object element() {
+            return this.element;
+        }
 
-		public Object clone() throws CloneNotSupportedException {
-			Maillon m = (Maillon) super.clone();
-			m.element = element;
-			return m;
-		}
-	}
+        public Object clone() throws CloneNotSupportedException {
+            Maillon m = (Maillon) super.clone();
+            m.element = element;
+            return m;
+        }
+    }
 
-	/**
-	 * Création d'une pile.
-	 * 
-	 * @param taille
-	 *            la taille de la pile, la taille doit être > 0
-	 */
-	public Pile4(int taille) {
-		if (taille <= 0)
+    /**
+     * Création d'une pile.
+     * 
+     * @param taille
+     *            la taille de la pile, la taille doit être > 0
+     */
+    public Pile4(int taille) {
+        if (taille <= 0)
                 taille =PileI.CAPACITE_PAR_DEFAUT;
-		this.stk = null;
-		this.capacite = taille;
-		this.nombre=0;
-	}
+        this.stk = null;
+        this.capacite = taille;
+        this.nombre=0;
+    }
 
-	public Pile4() {
-		this(PileI.CAPACITE_PAR_DEFAUT);
-	}
+    public Pile4() {
+        this(PileI.CAPACITE_PAR_DEFAUT);
+    }
 
-	public void empiler(Object o) throws PilePleineException {
-		if (estPleine())
-			throw new PilePleineException();
-		Maillon maillon=new Maillon(o,stk);
-		stk=maillon;
-		nombre++;
-	}
+    public void empiler(Object o) throws PilePleineException {
+        if (estPleine())
+            throw new PilePleineException();
+        Maillon maillon=new Maillon(o,stk);
+        stk=maillon;
+        nombre++;
+    }
 
-	public Object depiler() throws PileVideException {
-		if (estVide())
-			throw new PileVideException();
-		Object object=stk.element();
-		stk=stk.suivant;
-		nombre--;
-		return object;
-	}
+    public Object depiler() throws PileVideException {
+        if (estVide())
+            throw new PileVideException();
+        Object object=stk.element();
+        stk=stk.suivant;
+        nombre--;
+        return object;
+    }
 
-	public Object sommet() throws PileVideException {
-		if (estVide())
-			throw new PileVideException();
-		return stk.element();
-	}
+    public Object sommet() throws PileVideException {
+        if (estVide())
+            throw new PileVideException();
+        return stk.element();
+    }
 
-	/**
-	 * Effectue un test de l'état de la pile.
-	 * 
-	 * @return vrai si la pile est vide, faux autrement
-	 */
-	public boolean estVide() {
-		return stk==null;
-	}
+    /**
+     * Effectue un test de l'état de la pile.
+     * 
+     * @return vrai si la pile est vide, faux autrement
+     */
+    public boolean estVide() {
+        return stk==null;
+    }
 
-	/**
-	 * Effectue un test de l'état de la pile.
-	 * 
-	 * @return vrai si la pile est pleine, faux autrement
-	 */
-	public boolean estPleine() {
-		return nombre==capacite;
-	}
+    /**
+     * Effectue un test de l'état de la pile.
+     * 
+     * @return vrai si la pile est pleine, faux autrement
+     */
+    public boolean estPleine() {
+        return nombre==capacite;
+    }
 
-	/**
-	 * Retourne une représentation en String d'une pile, contenant la
-	 * représentation en String de chaque élément.
-	 * 
-	 * @return une représentation en String d'une pile
-	 */
-	public String toString() {
+    /**
+     * Retourne une représentation en String d'une pile, contenant la
+     * représentation en String de chaque élément.
+     * 
+     * @return une représentation en String d'une pile
+     */
+    public String toString() {
 
-		String s = "[";
-		Maillon maillon=stk;
-		while(stk!=null){
-		   s+=stk.element().toString();
-		   stk=stk.suivant();
-		   if(stk!=null)
-		   s+=", ";
-		  }
-		  stk=maillon;
-		return s + "]";
-	}
+        String s = "[";
+        Maillon maillon=stk;
+        while(stk!=null){
+           s+=stk.element().toString();
+           stk=stk.suivant();
+           if(stk!=null)
+           s+=", ";
+          }
+          stk=maillon;
+        return s + "]";
+    }
 
-	public boolean equals(Object o) {
-	  if(o instanceof Pile4){
+    public boolean equals(Object o) {
+      if(o instanceof Pile4){
            Pile4 p=(Pile4)o;
            
                return this.capacite() == p.capacite()
@@ -131,17 +131,17 @@ public class Pile4 implements PileI, Cloneable {
         else{
             return false;
         }
-	}
+    }
 
-	public int capacite() {
-		return this.capacite;
-	}
+    public int capacite() {
+        return this.capacite;
+    }
 
-	public int hashCode() {
-		return toString().hashCode();
-	}
+    public int hashCode() {
+        return toString().hashCode();
+    }
 
-	public int taille() {
-		return nombre;
-	}
+    public int taille() {
+        return nombre;
+    }
 }
